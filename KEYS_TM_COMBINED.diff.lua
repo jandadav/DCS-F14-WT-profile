07210ebfa329c2b2251fc7081f5dc665bff70b50
jandadav <janda.david@gmail.com>
2019-04-01 01:30:36 +0200

    Initial commit

---
+++ KEYS_TM_COMBINED.diff.lua
@@ -0,0 +1,21 @@
+local diff = {
+	["keyDiffs"] = {
+		["dnilp3020u3020cd17vdnilvp-1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN8",
+				},
+			},
+			["name"] = "Speed brake retract",
+		},
+		["dnilp3020u3020cd17vdnilvp1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_BTN7",
+				},
+			},
+			["name"] = "Speed brake extend",
+		},
+	},
+}
+return diff
