30a823de233f98f8c862a3a39d36876086b0d392
jandadav <janda.david@gmail.com>
2019-04-01 01:40:24 +0200

    diffs  Changes to be committed: 	new file:   KEYS_Keyboard.diff.lua 	modified:   KEYS_TM_COMBINED.diff.lua

---
+++ KEYS_Keyboard.diff.lua
@@ -0,0 +1,293 @@
+local diff = {
+	["keyDiffs"] = {
+		["d3024pnilu3024cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "3",
+					["reformers"] = {
+						[1] = "LAlt",
+					},
+				},
+			},
+			["name"] = "Wing Sweep Auto Mode",
+		},
+		["d3025pnilu3025cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "1",
+					["reformers"] = {
+						[1] = "LAlt",
+					},
+				},
+			},
+			["name"] = "Wing Sweep Forward",
+		},
+		["d3026pnilu3026cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "2",
+					["reformers"] = {
+						[1] = "LAlt",
+					},
+				},
+			},
+			["name"] = "Wing Sweep Aft",
+		},
+		["d3027pnilu3027cd16vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "4",
+					["reformers"] = {
+						[1] = "LAlt",
+					},
+				},
+			},
+			["name"] = "Wing Sweep Bomb Mode",
+		},
+		["d3041pnilunilcd21vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "A",
+					["reformers"] = {
+						[1] = "LCtrl",
+					},
+				},
+			},
+			["name"] = "Autopilot toggle",
+		},
+		["d3058pnilu3058cd22vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "R",
+				},
+			},
+			["name"] = "MASTER RESET",
+		},
+		["d3079pnilunilcd55vd-0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "9",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Weapon Selector Sidewinder Missiles",
+		},
+		["d3079pnilunilcd55vd-1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "0",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Weapon Selector Off",
+		},
+		["d3079pnilunilcd55vd0.5vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "-",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Weapon Selector Sparrow or Phoenix Missiles",
+		},
+		["d3079pnilunilcd55vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "8",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Weapon Selector Gun",
+		},
+		["d3082pnilu3082cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "7",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Weapon Selector press",
+		},
+		["d3084pnilu3084cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "/",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "DLC Toggle / Countermeasure Dispense",
+		},
+		["d3088pnilu3088cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "P",
+				},
+			},
+			["name"] = "Trigger First Detent",
+		},
+		["d3126pnilu3126cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "Space",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "CAGE-SEAM button",
+		},
+		["d3127pnilu3127cd57vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "9",
+					["reformers"] = {
+						[1] = "LAlt",
+					},
+				},
+			},
+			["name"] = "PLM button",
+		},
+		["d3137pnilu3137cd55vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "V",
+				},
+			},
+			["name"] = "Master Arm On and Cover Open, else Safe and Closed",
+		},
+		["d3137pnilunilcd55vd0vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "P",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "Master arm off and cover close",
+		},
+		["d3137pnilunilcd55vd1vpnilvunil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "P",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Master arm on and cover open",
+		},
+		["d3143pnilu3143cd55vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Target designate up / VSL HI",
+		},
+		["d3144pnilu3144cd55vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "/",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Target designate fwd / PAL",
+		},
+		["d3145pnilu3145cd55vd1vpnilvu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ".",
+					["reformers"] = {
+						[1] = "RAlt",
+					},
+				},
+			},
+			["name"] = "Target designate down / VSL LO",
+		},
+		["dnilp2019u2019cdnilvdnilvp-1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Trim Pitch Down",
+		},
+		["dnilp2019u2019cdnilvdnilvp1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ".",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Trim Pitch Up",
+		},
+		["dnilp2020u2020cdnilvdnilvp-1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ",",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Trim Roll Left Wing Down",
+		},
+		["dnilp2020u2020cdnilvdnilvp1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "/",
+					["reformers"] = {
+						[1] = "RCtrl",
+					},
+				},
+			},
+			["name"] = "Trim Roll Right Wing Down",
+		},
+		["dnilp3083u3083cd57vdnilvp-1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ".",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "DLC Thumbwheel Aft",
+		},
+		["dnilp3083u3083cd57vdnilvp1vu0"] = {
+			["added"] = {
+				[1] = {
+					["key"] = ";",
+					["reformers"] = {
+						[1] = "RShift",
+					},
+				},
+			},
+			["name"] = "DLC Thumbwheel Forward",
+		},
+	},
+}
+return diff
--- KEYS_TM_COMBINED.diff.lua
+++ KEYS_TM_COMBINED.diff.lua
@@ -1,4 +1,74 @@
 local diff = {
+	["axisDiffs"] = {
+		["a2001cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.16,
+						},
+						["deadzone"] = 0,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_Y",
+				},
+			},
+			["name"] = "Pitch",
+		},
+		["a2002cdnil"] = {
+			["changed"] = {
+				[1] = {
+					["filter"] = {
+						["curvature"] = {
+							[1] = 0.26,
+						},
+						["deadzone"] = 0.01,
+						["invert"] = false,
+						["saturationX"] = 1,
+						["saturationY"] = 1,
+						["slider"] = false,
+					},
+					["key"] = "JOY_X",
+				},
+			},
+			["name"] = "Roll",
+		},
+		["a2003cdnil"] = {
+			["name"] = "Rudder",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+		},
+		["a2004cdnil"] = {
+			["name"] = "Throttle Both",
+			["removed"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+		},
+		["a2005cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_RZ",
+				},
+			},
+			["name"] = "Throttle Left",
+		},
+		["a2006cdnil"] = {
+			["added"] = {
+				[1] = {
+					["key"] = "JOY_Z",
+				},
+			},
+			["name"] = "Throttle Right",
+		},
+	},
 	["keyDiffs"] = {
 		["dnilp3020u3020cd17vdnilvp-1vu0"] = {
 			["added"] = {
